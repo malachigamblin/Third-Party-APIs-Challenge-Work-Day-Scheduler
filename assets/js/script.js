e9a370d342dba9579227152d23b62ff21317fb8f
malachigamblin <malachi.gamblin@gmail.com>
2023-02-25 23:48:39 -0500

    added function to loop over time blocks to check for the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,5 +8,11 @@ $(document).ready(function () {
 
     localStorage.setItem(time, text);
   });
-});
 
+  function timeTracker() {
+    var timeNow = moment().hour();
+  }
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+  });
+});
