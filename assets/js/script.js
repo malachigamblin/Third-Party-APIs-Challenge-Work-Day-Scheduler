4f436bf7ce76d77eb693c8562f137b315c70c5b3
malachigamblin <malachi.gamblin@gmail.com>
2023-02-25 23:53:30 -0500

    added if statement to check the time then add/remove styles from CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,23 @@ $(document).ready(function () {
 
   function timeTracker() {
     var timeNow = moment().hour();
+
+    $(".time-block").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
   }
-  $(".time-block").each(function () {
-    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-  });
 });
